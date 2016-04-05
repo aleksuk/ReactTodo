0ef41acebc32ae5a02c6aca6f67c0c04683ad0cc
Mykhailo Aleksiuk <Mykhailo_Aleksiuk@epam.com>
2016-04-05 12:37:03 +0300

    Removed unusing code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainHeader from './components/main-header';
-import MainSection from './components/main-section';
 import Router from './routes';
 
 window.addEventListener('load', function () {
